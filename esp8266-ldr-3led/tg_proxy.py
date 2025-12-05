235576312cafbde34284113c4a236f90b0ffe0e2
nain.nz <gamesope0001@gmail.com>
2025-12-05 21:05:53 +0700

    Update tg_proxy.py

--- esp8266-ldr-3led/tg_proxy.py
+++ esp8266-ldr-3led/tg_proxy.py
@@ -6,7 +6,7 @@ import requests
 app = Flask(__name__)
 CORS(app)  # Enable CORS untuk semua route
 
-TELEGRAM_TOKEN = "8218255141:AAEmhiliqLRI7il0ZmqaMAboDHPDJ3bVPzo"
+TELEGRAM_TOKEN = "token bot telegram"
 
 
 @app.route('/', methods=['GET', 'POST', 'OPTIONS'])
